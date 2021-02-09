b885e64482a8c6c23fe22ff6dbbadbe96ba01bb3
김찬민 <rlacksals96@gmail.com>
2021-02-09 16:01:05 +0900

    add:sorting words
    -> 길이별로 sorting 먼저 해주고 (Arrays.sort(array,lambda)
    < ? super T> 꼴은 람다이므로 다음부터 잘 활용하기
    
    -> 출력의 경우 현재배열값과 이전 배열값 비교해서 체크하면 되므로 쉽게 생각하기

--- Baekjoon/src/정렬/수정렬하기2/QuickSorting.java
+++ Baekjoon/src/정렬/수정렬하기2/QuickSorting.java
@@ -3,8 +3,46 @@ package 정렬.수정렬하기2;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//dual pivot quick sort by using built-in method
 public class QuickSorting {
+    private static int[] result;
+
+    public static void merge(int[] arr,int m,int mid,int n){
+        int i=m;
+        int j=mid+1;
+        int k=m;
+
+        while(i<=mid && j<=n){
+            if(arr[i]<=arr[j]){
+                result[k]=arr[i];
+                i++;
+            }else{
+                result[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        if(i>mid){
+            for(int t=j;t<=n;t++) {
+                result[k] = arr[t];
+                k++;
+            }
+        }else{
+            for(int t=i;t<=mid;t++){
+                result[k]=arr[t];
+                k++;
+            }
+        }
+
+        if (n + 1 - m >= 0) System.arraycopy(result, m, arr, m, n + 1 - m);
+    }
+    public static void quickSort(int[] arr,int m,int n){
+        if(m<n){
+            int mid=(m+n)/2;
+            quickSort(arr,m,mid);
+            quickSort(arr,mid+1,n);
+            merge(arr,m,mid,n);
+        }
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
@@ -13,8 +51,11 @@ public class QuickSorting {
         for(int i=0;i<arr.length;i++)
             arr[i]=sc.nextInt();
 
-        Arrays.sort(arr);
+        result=new int[N];
+        quickSort(arr,0,arr.length-1);
+
         for(int i:arr)
             System.out.println(i);
+
     }
 }
