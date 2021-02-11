73b4ca20a3393f1fd602ac75a5f16b895cc0251c
kimchanmin <rlacksals96@gmail.com>
2021-02-11 14:47:16 +0900

    update

---
+++ Baekjoon/src/정렬/통계학/Main.java
@@ -0,0 +1,12 @@
+package 정렬.통계학;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
