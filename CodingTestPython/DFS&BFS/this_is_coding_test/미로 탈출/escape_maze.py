bcbf8a713f508d4b49aeda269c78eb072f6d9c6b
kimchanmin <rlacksals96@gmail.com>
2021-04-23 16:12:17 +0900

    add:dfs/bfs.escape_maze

---
+++ CodingTestPython/DFS&BFS/this_is_coding_test/미로 탈출/escape_maze.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+input = lambda: sys.stdin.readline().strip()
+
+N, M = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(N)]
+
+# N E S W
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[-1][-1]
+
+
+print(bfs(0, 0))
